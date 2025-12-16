c9e27c693dbfd7669bda1b54c08ddac56d46828e
DariaLaz <daria.lazarova03@gmail.com>
2025-12-16 02:11:02 +0200

    self

--- uni-coolc/cw3template/include/semantics/passes/TypeChecker.h
+++ uni-coolc/cw3template/include/semantics/passes/TypeChecker.h
@@ -4,26 +4,35 @@
 #include <any>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 #include "CoolParser.h"
 #include "CoolParserBaseVisitor.h"
 
-class TypeChecker : public CoolParserBaseVisitor {
-  private:
-    // define any necessary fields
-    std::vector<std::string> errors;
+class TypeChecker : public CoolParserBaseVisitor
+{
+private:
+  // define any necessary fields
+  std::vector<std::string> errors;
 
-    // override necessary visitor methods
+  // override necessary visitor methods
 
-    // define helper methods
+  // define helper methods
+  std::string current_class;
+  std::unordered_set<std::string> visitedMethods;
 
-  public:
-    // TODO: add necessary dependencies to constructor
-    TypeChecker() {}
+public:
+  // TODO: add necessary dependencies to constructor
+  TypeChecker() {}
 
-    // Typechecks the AST that the parser produces and returns a list of errors,
-    // if any.
-    std::vector<std::string> check(CoolParser *parser);
+  // Typechecks the AST that the parser produces and returns a list of errors,
+  // if any.
+  std::vector<std::string> check(CoolParser *parser);
+
+  std::any visitClass(CoolParser::ClassContext *ctx) override;
+  std::any visitMethod(CoolParser::MethodContext *ctx) override;
+
+  std::any visitExpr(CoolParser::ExprContext *ctx) override;
 };
 
 #endif
--- uni-coolc/cw3template/src/semantics/passes/TypeChecker.cpp
+++ uni-coolc/cw3template/src/semantics/passes/TypeChecker.cpp
@@ -7,9 +7,52 @@
 
 using namespace std;
 
-vector<string> TypeChecker::check(CoolParser *parser) {
+vector<string> TypeChecker::check(CoolParser *parser)
+{
     visitProgram(parser->program());
     parser->reset();
 
     return std::move(errors);
+}
+
+std::any TypeChecker::visitClass(CoolParser::ClassContext *ctx)
+{
+    current_class = ctx->TYPEID(0)->getText();
+    visitedMethods.clear();
+    return visitChildren(ctx);
+}
+
+std::any TypeChecker::visitMethod(CoolParser::MethodContext *ctx)
+{
+    string methodName = ctx->OBJECTID()->getText();
+    string declaredReturnType = ctx->TYPEID()->getText();
+
+    any bodyAny = visit(ctx->expr());
+
+    string bodyType = "any";
+    if (bodyAny.has_value() && bodyAny.type() == typeid(string))
+        bodyType = any_cast<string>(bodyAny);
+
+    if (bodyType == "self" && declaredReturnType != current_class && !visitedMethods.count(methodName))
+    {
+        errors.push_back(
+            "In class `" + current_class +
+            "` method `" + methodName +
+            "`: `SELF_TYPE` is not `" + declaredReturnType +
+            "`: type of method body is not a subtype of return type");
+    }
+
+    visitedMethods.insert(methodName);
+
+    return nullptr;
+}
+
+std::any TypeChecker::visitExpr(CoolParser::ExprContext *ctx)
+{
+    if (!ctx->OBJECTID().empty() && ctx->OBJECTID(0)->getText() == "self")
+    {
+        return std::any{std::string{"self"}};
+    }
+
+    return visitChildren(ctx);
 }
